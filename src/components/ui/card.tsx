95e565b3f81d98679ec7b3c1d43270a71aad2bc9
soshi1234 <148639191+soshi1234@users.noreply.github.com>
2024-12-06 11:31:19 +0900

    12/1終了時点

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import { mockDataItems } from "@/mocks/product";
 
-export function Card() {
-    return <a href={mockDataItems[0].Item.itemUrl} className="h-[300px]">
-        <Image
-            src={mockDataItems[0].Item.mediumImageUrls[0].imageUrl}
-            alt={mockDataItems[0].Item.itemName}
-            width={128}
-            height={128} />
-        <h2 className="text-center text-sm mt-2 w-32 line-clamp-5">
-            {mockDataItems[0].Item.itemName}
-        </h2>
-    </a>;
+export function Card(props) {
+  return (
+    <a href={props.url} className="h-[300px]">
+      <Image
+        src={props.image}
+        alt={mockDataItems[0].Item.itemName}
+        width={128}
+        height={128}
+      />
+      <h2 className="text-center text-sm mt-2 w-32 line-clamp-5">
+        {props.name}
+      </h2>
+    </a>
+  );
 }
